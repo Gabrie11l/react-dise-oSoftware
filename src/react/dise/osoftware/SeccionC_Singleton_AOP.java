f24df4ed4b1a52b94d262f0af4f1df6d815edc09
Jared <joaquito@espol.edu.ec>
2026-06-07 21:03:20 -0500

    Separacion de main por secciones

--- src/react/dise/osoftware/SeccionC_Singleton_AOP.java
+++ src/react/dise/osoftware/SeccionC_Singleton_AOP.java
@@ -1,18 +1,22 @@
 package react.dise.osoftware;
 
 import react.dise.osoftware.sectionC_aop_singleton.ConfiguracionAOP;
+import react.dise.osoftware.sectionC_aop_singleton.IConfiguracion;
+import react.dise.osoftware.sectionC_aop_singleton.Singleton;
 
 public class SeccionC_Singleton_AOP {
     static void main(String[] args) {
         System.out.println("----- PRUEBA SECCION C: SINGLETON CON AOP -----");
+        //SECCION C
+        
         //Primer Modulo
         System.out.println("Modulo de autenticacion");
-        ConfiguracionAOP confi1 = new ConfiguracionAOP();
+        IConfiguracion confi1 = Singleton.getInstancia();
         confi1.mostrarMensaje();
-
+        
         //Segundo modulo
         System.out.println("Modulo de reportes");
-        ConfiguracionAOP confi2 = new ConfiguracionAOP();
+        IConfiguracion confi2 = Singleton.getInstancia();
         confi2.mostrarMensaje();
         System.out.println("¿Son la misma instancia?: " + (confi1 == confi2));
     }
